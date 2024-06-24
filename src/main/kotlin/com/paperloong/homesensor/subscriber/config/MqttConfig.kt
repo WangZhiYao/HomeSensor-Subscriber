4b1840abac7510d2857cb0ec8fa1cf236ba74b59
WangZhiYao <393893729@qq.com>
2024-06-24 20:17:29 +0800

    Update: remove subscriber mqtt outbound
    Signed-off-by: WangZhiYao <393893729@qq.com>

--- src/main/kotlin/com/paperloong/homesensor/subscriber/config/MqttConfig.kt
+++ src/main/kotlin/com/paperloong/homesensor/subscriber/config/MqttConfig.kt
@@ -3,19 +3,15 @@ package com.paperloong.homesensor.subscriber.config
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.integration.annotation.MessagingGateway
 import org.springframework.integration.annotation.ServiceActivator
 import org.springframework.integration.channel.DirectChannel
 import org.springframework.integration.core.MessageProducer
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter
-import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter
-import org.springframework.integration.mqtt.support.MqttHeaders
 import org.springframework.messaging.MessageChannel
 import org.springframework.messaging.MessageHandler
-import org.springframework.messaging.handler.annotation.Header
 
 /**
  *
@@ -65,24 +61,5 @@ data class MqttConfig(
     @ServiceActivator(inputChannel = "mqttInputChannel")
     fun handler(): MessageHandler {
         return messageHandler
-    }
-
-    @Bean
-    fun mqttOutboundChannel(): MessageChannel {
-        return DirectChannel()
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "mqttOutboundChannel")
-    fun outbound(): MessageHandler {
-        val messageHandler = MqttPahoMessageHandler(properties.clientId, mqttClientFactory())
-        messageHandler.setAsync(true)
-        messageHandler.setDefaultTopic("test")
-        return messageHandler
-    }
-
-    @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
-    interface MqttGateway {
-        fun sendToMqtt(@Header(MqttHeaders.TOPIC) topic: String, message: String)
     }
 }
