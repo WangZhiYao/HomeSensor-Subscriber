1497e2f3a5fa330aba70c9147bd1353ec07edafe
WangZhiYao <393893729@qq.com>
2024-07-14 19:10:12 +0800

    Update: extract data type

--- models.py
+++ models.py
@@ -1,14 +1,20 @@
+from enum import StrEnum
 from typing import Dict, Any
 
 from beanie import Document, Indexed
 from pydantic import Field
 
 
+class DataType(StrEnum):
+    THP = 'thp'
+    LIGHT = 'light'
+
+
 class SensorData(Document):
     sensor_id: Indexed(str)
-    type: str
-    timestamp: int
+    type: DataType
     data: Dict[str, Any] = Field(default_factory=dict)
+    timestamp: int
 
     class Settings:
         name = 'sensor_data'
